1ec11b242d7a7cae08603cfdff71cfe87abe3b64
tungnb <tung.nb2611@gmail.com>
2022-04-17 19:37:32 +0700

    the first

---
+++ backend/src/main/java/com/sapo/QuanLyBanner/QuanLyBannerApplication.java
@@ -0,0 +1,13 @@
+package com.sapo.QuanLyBanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyBannerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sapo/QuanLyBanner/QuanLyBannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapo.QuanLyBanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyBannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
