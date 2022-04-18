a10628d1d3510d2f142c43dd972cdc81a68d8740
tungnb <tung.nb2611@gmail.com>
2022-04-18 15:31:50 +0700

    add folder

---
+++ backend/src/main/java/com/sapo/QuanLyBanner/entities/BannerEntity.java
@@ -0,0 +1,12 @@
+package com.sapo.QuanLyBanner.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name =" banners")
+
+public class BannerEntity {
+
+
+}
---
+++ backend/src/main/java/com/sapo/QuanLyBanner/entities/UserEntity.java
@@ -0,0 +1,4 @@
+package com.sapo.QuanLyBanner.entities;
+
+public class UserEntity {
+}
