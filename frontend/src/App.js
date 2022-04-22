56efaebd304d8ed18284a6b95299569fec3946f7
Le Ngoc Kien <lengockien2000@gmail.com>
2022-04-22 09:46:36 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,44 +4,183 @@ import Layout from './components/Layout';
 import styled from "styled-components";
 import { BrowserRouter as Router, Switch, Route,Link } from "react-router-dom";
 import './index.css'
+import AuthService from "./services/auth";
 
+import Login from "./components/login";
+import Register from "./components/register";
+import Home from "./components/home";
+import Profile from "./components/profile";
+import BoardUser from "./components/board-user";
+import BoardModerator from "./components/board-moderator";
+import BoardAdmin from "./components/board-admin";
 import Login from './components/login/Login';
 
-function App({userInfo}) {
-  return (
-    <>
-      {/* <Home>
-        <Route exact path="/login">
-          <Login/>
-        </Route> */}
-        {/* <Layout>
-          <Switch>
-            <Route exact path="/">
-              <AboutUs/>
-            </Route>
-            <Route exact path="/contact">
-              <Contact/>
-            </Route>
-          </Switch>
-        </Layout> */}
-      {/* </Home> */}
-      <Router>
-          <div className="outer">
-            <div className="inner">
-              <Switch>
-                <Route exact path='/' component={Login} />
-                <Route path="/sign-in" component={Login} />
-              </Switch>
-            </div>
+// function App() {
+//   return (
+//     <>
+//       {/* <Home>
+//         <Route exact path="/login">
+//           <Login/>
+//         </Route> */}
+//         {/* <Layout>
+//           <Switch>
+//             <Route exact path="/">
+//               <AboutUs/>
+//             </Route>
+//             <Route exact path="/contact">
+//               <Contact/>
+//             </Route>
+//           </Switch>
+//         </Layout> */}
+//       {/* </Home> */}
+//       <Router>
+//           <div className="outer">
+//             <div className="inner">
+//               <Switch>
+//                 <Route exact path='/' component={Login} />
+//                 <Route path="/sign-in" component={Login} />
+//               </Switch>
+//             </div>
+//           </div>
+//       </Router>
+//     </>  
+//   );
+// }
+
+// // export const Home = styled.div `
+// //   display: flex;
+// // `;
+
+// export default App;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    };
+  }
+
+  componentDidMount() {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      this.setState({
+        currentUser: user,
+        showModeratorBoard: user.roles.includes("ROLE_MODERATOR"),
+        showAdminBoard: user.roles.includes("ROLE_ADMIN"),
+      });
+    }
+    
+    EventBus.on("logout", () => {
+      this.logOut();
+    });
+  }
+
+  componentWillUnmount() {
+    EventBus.remove("logout");
+  }
+
+  logOut() {
+    AuthService.logout();
+    this.setState({
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    });
+  }
+
+  render() {
+    const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/"} className="navbar-brand">
+            bezKoder
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/home"} className="nav-link">
+                Home
+              </Link>
+            </li>
+
+            {showModeratorBoard && (
+              <li className="nav-item">
+                <Link to={"/mod"} className="nav-link">
+                  Moderator Board
+                </Link>
+              </li>
+            )}
+
+            {showAdminBoard && (
+              <li className="nav-item">
+                <Link to={"/admin"} className="nav-link">
+                  Admin Board
+                </Link>
+              </li>
+            )}
+
+            {currentUser && (
+              <li className="nav-item">
+                <Link to={"/user"} className="nav-link">
+                  User
+                </Link>
+              </li>
+            )}
           </div>
-      </Router>
-    </>  
-  );
+
+          {currentUser ? (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/profile"} className="nav-link">
+                  {currentUser.username}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link" onClick={this.logOut}>
+                  LogOut
+                </a>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to={"/register"} className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </div>
+          )}
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/home"]} component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/profile" component={Profile} />
+            <Route path="/user" component={BoardUser} />
+            <Route path="/mod" component={BoardModerator} />
+            <Route path="/admin" component={BoardAdmin} />
+          </Switch>
+        </div>
+
+        { /*<AuthVerify logOut={this.logOut}/> */ }
+      </div>
+    );
+  }
 }
-export const Home = styled.div `
-  display: flex;
-`;
 
 export default App;
-
-
